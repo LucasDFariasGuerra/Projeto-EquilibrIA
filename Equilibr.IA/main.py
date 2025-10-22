2580f9a84392322aeff943964be9718f391021c4
LucasDFariasGuerra <lucas.fariasguerra@ufrpe.br>
2025-10-22 10:45:17 -0300

    Atualizações importantes em  main.py

--- Equilibr.IA/main.py
+++ Equilibr.IA/main.py
@@ -5,7 +5,6 @@ import utils
 def main():
     """Função principal que executa o loop do aplicativo."""
     
-    # Variável para controlar qual usuário está logado no momento
     usuario_logado = None
 
     while True:
@@ -18,7 +17,6 @@ def main():
                 utils.pausar_tela()
             
             elif escolha == '2':
-                # A função verificar_login retorna o username se o login for válido
                 username_valido = user_manager.verificar_login()
                 if username_valido:
                     usuario_logado = username_valido
@@ -31,7 +29,7 @@ def main():
             else:
                 print(utils.COR_ERRO + "\n Opção inválida. Por favor, tente novamente.")
 
-        # Se UM usuário ESTIVER logado, exibe o menu de usuário
+        #Menu de usuário
         else:
             escolha = interface.exibir_menu_logado(usuario_logado)
             
@@ -41,25 +39,25 @@ def main():
                 utils.pausar_tela()
             
             elif escolha == '2':
-                # Chama a nova função de edição
+                #Edição de conta
                 user_manager.editar_usuario(usuario_logado)
                 utils.pausar_tela()
             
             elif escolha == '3':
                 user_manager.excluir_usuario()
-                # Após excluir, o usuário é deslogado
+                #Exclusão de conta 
                 usuario_logado = None
                 utils.pausar_tela()
             
             elif escolha == '0':
                 print(utils.COR_SUCESSO + "\nFazendo logout...")
-                usuario_logado = None # Desloga o usuário
+                usuario_logado = None #Desloga o usuário
                 utils.pausar_tela()
             
             else:
                 print(utils.COR_ERRO + "\n❌ Opção inválida. Por favor, tente novamente.")
                 utils.pausar_tela()
 
-# Garante que a função main() só será executada quando este arquivo for o principal
+#Garante que a função main() só será executada quando este arquivo for o principal
 if __name__ == "__main__":
     main()
